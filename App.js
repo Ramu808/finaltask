fb0d831f15a6497d110a5f5f8c158088752a961b
Ramu <ramaiahudayagiri89@gmail.com>
2021-05-31 11:51:17 +0530

    Ramu added finaltask

---
+++ App.js
@@ -0,0 +1,17 @@
+//login screen
+import React, {Component} from 'react';
+import {View, Text, Image,StyleSheet, Button, TextInput, TouchableOpacity} from 'react-native';
+
+export default class Home extends Component {
+
+
+
+render() {
+  return(
+    <View>
+      <Text style={{fontSize:19,marginLeft:100}}>Hiii This is final Projecet</Text>
+    </View>
+
+  );
+};
+}
